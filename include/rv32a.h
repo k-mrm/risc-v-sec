4eb89b4c2a28ce8d3bd655fdf9ba4c549cb740ef
mrm <kei.id0305@gmail.com>
2021-01-03 23:18:26 +0900

    fix decoder bug

--- include/rv32a.h
+++ include/rv32a.h
@@ -3,6 +3,23 @@
 
 #include "cpu.h"
 
+#define RL(op)  (((op) >> 25) & 1)
+#define AQ(op)  (((op) >> 26) & 1)
+#define FUNCT5(op)  (((op) >> 27) & 0x1f)
+
+#define AMO   0b0101111
+#define OP_AMOSWAP  0b00001
+#define OP_AMOADD 0b00000
+#define OP_AMOXOR 0b00100
+#define OP_AMOAND 0b01100
+#define OP_AMOOR  0b01000
+#define OP_AMOMIN 0b10000
+#define OP_AMOMAX 0b10100
+#define OP_AMOMINU  0b11000
+#define OP_AMOMAXU  0b11100
+
+void rv32a_lr_w(struct cpu *cpu, uint8_t rd, uint8_t rs1);
+void rv32a_sc_w(struct cpu *cpu, uint8_t rd, uint8_t rs1, uint8_t rs2);
 void rv32a_amoadd_w(struct cpu *cpu, uint8_t rd, uint8_t rs1, uint8_t rs2);
 void rv32a_amoand_w(struct cpu *cpu, uint8_t rd, uint8_t rs1, uint8_t rs2);
 void rv32a_amomax_w(struct cpu *cpu, uint8_t rd, uint8_t rs1, uint8_t rs2);
