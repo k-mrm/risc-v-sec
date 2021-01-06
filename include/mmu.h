3fffd9ed96b67103aa33a0fe02c7f3eb989536c4
mrm <kei.id0305@gmail.com>
2021-01-06 18:16:38 +0900

    update MMU

--- include/mmu.h
+++ include/mmu.h
@@ -1,10 +1,23 @@
 #ifndef RISC_V_SEC_MMU_H
 #define RISC_V_SEC_MMU_H
 
+#include "cpu.h"
 #include "csr.h"
+
+/* sv39 */
 
 #define SATP_PPN(satp)  ((satp) & 0xfffffffffff)
 #define SATP_ASID(satp) (((satp) >> 43) & 0xffff)
 #define SATP_MODE(satp) (((satp) >> 59) & 0xf)
+
+#define PAGE_OFFSET(va) ((va) & 0xfff)
+#define VPN0(va)  (((va) >> 12) & 0x1ff)
+#define VPN1(va)  (((va) >> 21) & 0x1ff)
+#define VPN2(va)  (((va) >> 30) & 0x1ff)
+
+#define PAGESIZE  4096
+
+#define LEVELS  3
+#define PTESIZE 8
 
 #endif
--- src/mmu.c
+++ src/mmu.c
@@ -1,1 +1,12 @@
 #include "mmu.h"
+
+reg_t sv39_translate(struct cpu *cpu, reg_t vaddr) {
+  reg_t offset = PAGE_OFFSET(vaddr);
+  reg_t vpn[3] = { VPN0(vaddr), VPN1(vaddr), VPN2(vaddr) };
+  reg_t satp = csrread(cpu->csrs, SATP);
+
+  reg_t a = SATP_PPN(satp) * PAGESIZE;
+  reg_t i = LEVELS - 1;
+
+  reg_t pte = sysbus_read64(cpu->bus, a + vpn[i] * PTESIZE);
+}
