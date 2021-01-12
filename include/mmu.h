31aeb4dc9b8944ee1c15538c771368659299325d
mrm <kei.id0305@gmail.com>
2021-01-13 08:49:55 +0900

    implement MMU

--- include/mmu.h
+++ include/mmu.h
@@ -9,8 +9,8 @@
 /* Sv39 */
 
 #define SATP_PPN(satp)  ((satp) & 0xfffffffffff)
-#define SATP_ASID(satp) (((satp) >> 43) & 0xffff)
-#define SATP_MODE(satp) (((satp) >> 59) & 0xf)
+#define SATP_ASID(satp) (((satp) >> 44) & 0xffff)
+#define SATP_MODE(satp) (((satp) >> 60) & 0xf)
 
 #define PAGE_OFFSET(va) ((va) & 0xfff)
 #define VPN0(va)  (((va) >> 12) & 0x1ff)
@@ -25,6 +25,32 @@
 #elif XLEN == 32
 /* Sv32 */
 
+#define SATP_PPN(satp)  ((satp) & 0x3fffff)
+#define SATP_ASID(satp)  (((satp) >> 22) & 0x1ff)
+#define SATP_MODE(satp)  (((satp) >> 31) & 0x1)
+
+#define PAGE_OFFSET(va) ((va) & 0xfff)
+#define VPN0(va)  (((va) >> 12) & 0x3ff)
+#define VPN1(va)  (((va) >> 22) & 0x3ff)
+
+#define PAGESIZE 4096
+#define LEVELS 2
+#define PTESIZE 4
+
+#define PTE_RSW(p)  (((p) >> 8) & 0x3)
+#define PTE_PPN(p)  (((p) >> 10) & 0x3fffff)
+#define PTE_PPN0(p)  (((p) >> 10) & 0x3ff)
+#define PTE_PPN1(p)  (((p) >> 20) & 0xfff)
+
 #endif
+
+#define PTE_V(p)  ((p) & 0x1)
+#define PTE_R(p)  (((p) >> 1) & 0x1)
+#define PTE_W(p)  (((p) >> 2) & 0x1)
+#define PTE_X(p)  (((p) >> 3) & 0x1)
+#define PTE_U(p)  (((p) >> 4) & 0x1)
+#define PTE_G(p)  (((p) >> 5) & 0x1)
+#define PTE_A(p)  (((p) >> 6) & 0x1)
+#define PTE_D(p)  (((p) >> 7) & 0x1)
 
 #endif
--- src/mmu.c
+++ src/mmu.c
@@ -1,7 +1,36 @@
 #include "mmu.h"
 
 reg_t sv32_translate(struct cpu *cpu, reg_t vaddr) {
-  ;
+  reg_t offset = PAGE_OFFSET(vaddr);
+  reg_t vpn[2] = { VPN0(vaddr), VPN1(vaddr) };
+  reg_t satp = csrread(cpu->csrs, SATP);
+
+  reg_t a = SATP_PPN(satp) * PAGESIZE;
+  reg_t i = LEVELS - 1;
+
+  while(1) {
+    reg_t pte = sysbus_read32(cpu->bus, a + vpn[i] * PTESIZE);
+
+    if(PTE_R(pte) == 1 && PTE_X(pte) == 1) {
+      break;
+    }
+    i--;
+    a = PTE_PPN(pte) * PAGESIZE;
+  }
+
+  reg_t pa_pgoff = PAGE_OFFSET(vaddr);
+  reg_t pte_ppn[2] = { PTE_PPN0(pte), PTE_PPN1(pte) };
+  reg_t ppn[2];
+  if(i > 0) {
+    for(int j = 0; j <= i - 1; j++) {
+      ppn[j] = vpn[j];
+    }
+  }
+  for(int j = i; j <= LEVELS - 1; j++) {
+    ppn[j] = pte_ppn[j];
+  }
+
+  return (ppn[1] << 22) | (ppn[0] << 12) | offset;
 }
 
 reg_t sv39_translate(struct cpu *cpu, reg_t vaddr) {
